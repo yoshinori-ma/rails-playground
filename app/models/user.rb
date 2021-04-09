187d67c2e62210a355f125bf228f54a2fdd32068
yoshinori maeda <yoshinori@collab-it.net>
2021-04-09 14:20:32 +0900

    Add: 面白い挙動

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@
 class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, presence: true
   has_many :posts, dependent: :destroy
+
+  # シンボルで渡した場合、オーバーライドする
+  # after_create_commit が実行されない
+  # after_create_commit :log_user_saved_to_db
+  # after_update_commit :log_user_saved_to_db
+
+  # ブロックで渡した場合、両方実行される
+  after_create_commit { log_user_saved_to_db }
+  after_update_commit { log_user_saved_to_db }
+
+  private
+
+  def log_user_saved_to_db
+    puts 'User was saved to database'
+  end
 end
