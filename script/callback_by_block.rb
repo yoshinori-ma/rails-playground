21c7a3b82c0d97ca51c33a2e0fc0215e9b388b9b
yoshinori maeda <yoshinori@collab-it.net>
2021-04-09 15:53:58 +0900

    Add: 検証用スクリプト

---
+++ script/callback_by_block.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+ActiveRecord::Migration[6.1].new
+                            .create_table :cats, &:timestamps
+
+class Cat < ApplicationRecord
+  after_create_commit { log_cat_saved_to_db }
+  after_update_commit { log_cat_saved_to_db }
+
+  private
+
+  def log_cat_saved_to_db
+    puts 'Cat was saved to database'
+  end
+end
+
+puts 'create start'
+# 何も出力されないと思ったらされる
+cat = Cat.create
+puts 'create end'
+
+puts 'update start'
+# catを更新する
+# こっちもされる
+cat.save
+puts 'update end'
+
+ActiveRecord::Migration[6.1].new.drop_table :cats
